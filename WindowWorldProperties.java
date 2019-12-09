d64308c26085b761d4fb5579bb3e2a86040c621d
Eric Sjöström <eric.seastream@gmail.com>
2019-12-09 11:11:17 +0100

    Renaming of file

---
+++ WindowWorldProperties.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class HelloWorld extends Canvas {
+    static Frame frame = new Frame();
+}
+
+public class WindowWorldProperties {
+    public static void main(String[] args) {
+        HelloWorld window = new HelloWorld();
+    }
+}
--- balls.java
+++
@@ -1,5 +0,0 @@
-public class balls {
-    public static void main(String[] args) {
-        
-    }
-}
